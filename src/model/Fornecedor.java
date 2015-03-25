c64205a428da41f90f23afbab2af5e75661ae73c
Matheus S. Pereira <matheussilva.fga@gmail.com>
2015-03-25 19:42:32 -0300

    applying indentation in Fornecedor.java based in the stylesheet

--- src/model/Fornecedor.java
+++ src/model/Fornecedor.java
@@ -3,61 +3,75 @@ package model;
 
 import java.util.ArrayList;
 
+public class Fornecedor
+{
+	protected String nome;
+	protected String telefone;
+	protected String celular;
+	protected ArrayList<Produto> listaProduto;
+	protected Endereco endereco;
 
-public class Fornecedor {
-    protected String nome;
-    protected String telefone;
-    protected String celular;
-    protected ArrayList<Produto> listaProduto;
-    protected Endereco endereco;
+	public Fornecedor(String nome,
+					  String telefone,
+					  String celular,
+					  ArrayList<Produto> listaProduto,
+					  Endereco endereco)
+	{
+		this.nome = nome;
+		this.telefone = telefone;
+		this.celular = celular;
+		this.listaProduto = listaProduto;
+		this.endereco = endereco;
+	}
 
-    public Fornecedor(String nome, String telefone, String celular, ArrayList<Produto> listaProduto, Endereco endereco) {
-        this.nome = nome;
-        this.telefone = telefone;
-        this.celular = celular;
-        this.listaProduto = listaProduto;
-        this.endereco = endereco;
-    }
+	public String getTelefone()
+	{
+		return telefone;
+	}
 
-    public String getTelefone() {
-        return telefone;
-    }
+	public void setTelefone(String telefone)
+	{
+		this.telefone = telefone;
+	}
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+	public String getCelular()
+	{
+		return celular;
+	}
 
-    public String getCelular() {
-        return celular;
-    }
+	public void setCelular(String celular)
+	{
+		this.celular = celular;
+	}
 
-    public void setCelular(String celular) {
-        this.celular = celular;
-    }
+	public String getNome()
+	{
+		return nome;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public void setNome(String nome)
+	{
+		this.nome = nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public ArrayList<Produto> getListaProduto()
+	{
+		return listaProduto;
+	}
 
-    public ArrayList<Produto> getListaProduto() {
-        return listaProduto;
-    }
+	public void setListaProduto(ArrayList<Produto> listaProduto)
+	{
+		this.listaProduto = listaProduto;
+	}
 
-    public void setListaProduto(ArrayList<Produto> listaProduto) {
-        this.listaProduto = listaProduto;
-    }
+	public Endereco getEndereco()
+	{
+		return endereco;
+	}
 
-    public Endereco getEndereco() {
-        return endereco;
-    }
+	public void setEndereco(Endereco endereco)
+	{
+		this.endereco = endereco;
+	}
 
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-    
-    
 }

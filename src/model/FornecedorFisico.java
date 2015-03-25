25d285346fae3fab104068a2156b5e85f7913696
Matheus S. Pereira <matheussilva.fga@gmail.com>
2015-03-25 19:43:36 -0300

    applying indentation in FornecedorFisico.java based in the stylesheet

--- src/model/FornecedorFisico.java
+++ src/model/FornecedorFisico.java
@@ -1,33 +1,43 @@
-
 package model;
 
 import java.util.ArrayList;
 
-public class FornecedorFisico extends Fornecedor{
-    private String cpf;
-    private String rg;
+public class FornecedorFisico extends Fornecedor
+{
+	private String cpf;
+	private String rg;
 
-    public FornecedorFisico(String cpf, String rg, String nome, String telefone, String celular, ArrayList<Produto> listaProduto, Endereco umEndereco) {
-        super(nome, telefone, celular, listaProduto, umEndereco);
-        this.cpf = cpf;
-        this.rg = rg;
-    }
+	public FornecedorFisico(String cpf,
+							String rg,
+							String nome,
+							String telefone,
+							String celular,
+							ArrayList<Produto> listaProduto,
+							Endereco umEndereco)
+	{
+		super(nome, telefone, celular, listaProduto, umEndereco);
+		this.cpf = cpf;
+		this.rg = rg;
+	}
 
-    public String getCpf() {
-        return cpf;
-    }
+	public String getCpf()
+	{
+		return cpf;
+	}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	public void setCpf(String cpf)
+	{
+		this.cpf = cpf;
+	}
 
-    public String getRg() {
-        return rg;
-    }
+	public String getRg()
+	{
+		return rg;
+	}
 
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-    
-    
+	public void setRg(String rg)
+	{
+		this.rg = rg;
+	}
+
 }

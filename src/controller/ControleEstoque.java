b7af30222df6ff7996118fbc815b57b5d6d73f2d
Omar Júnior <omarroinuj@gmail.com>
2015-03-25 15:38:19 -0300

    Identing ControleEstoque class code

--- src/controller/ControleEstoque.java
+++ src/controller/ControleEstoque.java
@@ -1,33 +1,41 @@
 package controller;
+
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import model.Produto;
 import view.TelaEstoque;
 
-public class ControleEstoque {
+public class ControleEstoque
+{
     TelaEstoque telaEstoque;
     private ArrayList<Produto> listaProdutos;
     
-    public ControleEstoque(){
+    public ControleEstoque()
+    {
         this.listaProdutos = new ArrayList<Produto>();
     }
 
-    public ArrayList<Produto> getListaProdutos() {
+    public ArrayList<Produto> getListaProdutos()
+    {
         return listaProdutos;
     }
 
-    public void setListaProdutos(ArrayList<Produto> listaProdutos) {
+    public void setListaProdutos(ArrayList<Produto> listaProdutos)
+    {
         this.listaProdutos = listaProdutos;
     }
     
-    public void adicionarProduto(Produto p){
+    public void adicionarProduto(Produto p)
+    {
         listaProdutos.add(p);
     }
-    public void removerProduto(Produto p){
+    public void removerProduto(Produto p)
+    {
         listaProdutos.remove(p);
     }
     
-    public Produto pesquisarProduto(String nome, boolean pesquisa){
+    public Produto pesquisarProduto(String nome, boolean pesquisa)
+    {
         int i=0;
         Produto volta = null;
         Produto voltaExata = null;
@@ -38,29 +46,42 @@ public class ControleEstoque {
                 i++;
                 volta = p;
             }
+            
             if(p.getCodigo().equalsIgnoreCase(nome))
             {
                 voltaExata = p;
             }
+            
             if(p.getDescricao().toLowerCase().contains(nome.toLowerCase())&&pesquisa==true)
             {
                 volta = p;
                 i++;
             }
+            
             if(p.getDescricao().equalsIgnoreCase(nome))
             {
                 voltaExata = p;
             }
+            
         }
+        
         if(i>1&&pesquisa==true)
+        {
             JOptionPane.showMessageDialog(telaEstoque, "Mais de um resultado encontrado","Atenção" ,JOptionPane.INFORMATION_MESSAGE);
-
+        }
+        
         if(voltaExata != null)
+        {
             return voltaExata;
+        }
         
         else if(volta != null)
+        {
             return volta;
+        }
         
         return null;
+        
     }
+    
 }

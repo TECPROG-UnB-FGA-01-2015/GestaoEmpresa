0d1f008ee53f3f6a5ca1ba3125bda6bf6a2f2ade
Omar Júnior <omarroinuj@gmail.com>
2015-03-25 15:30:34 -0300

    Identing ControleDespesa class code

--- src/controller/ControleDespesa.java
+++ src/controller/ControleDespesa.java
@@ -9,29 +9,37 @@ package controller;
 import java.util.ArrayList;
 import model.Despesa;
 
-public class ControleDespesa {
+public class ControleDespesa
+{
     private ArrayList<Despesa> listaGasto;
 
-    public ControleDespesa() {
+    public ControleDespesa()
+    {
         this.listaGasto = new ArrayList<Despesa>();
     }
 
-    public ArrayList<Despesa> getListaGasto() {
+    public ArrayList<Despesa> getListaGasto()
+    {
         return listaGasto;
     }
 
-    public void setListaGasto(ArrayList<Despesa> listaGasto) {
+    public void setListaGasto(ArrayList<Despesa> listaGasto)
+    {
         this.listaGasto = listaGasto;
     }
     
-    public void adicionarGasto(Despesa g){
+    public void adicionarGasto(Despesa g)
+    {
         listaGasto.add(g);
     }
-    public void removerGasto(Despesa g){
+    
+    public void removerGasto(Despesa g)
+    {
         listaGasto.remove(g);
     }
     
-    public Despesa pesquisarGasto(String nome){
+    public Despesa pesquisarGasto(String nome)
+    {
         for(Despesa g: listaGasto)
             if(g.getNome().equalsIgnoreCase(nome))
                 return g;

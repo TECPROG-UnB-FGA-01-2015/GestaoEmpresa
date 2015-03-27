e46144479fa30600c4e356cfda00cbeee34172bf
Eduardo Moreira <moreira.edu@hotmail.com>
2015-03-27 08:59:25 -0300

    Applying identation and changing variables names on PhysicalClient model

--- src/model/ClienteFisico.java
+++ src/model/ClienteFisico.java
@@ -1,34 +1,39 @@
-
 package model;
 
-
-public class ClienteFisico extends Cliente {
-    
+public class PhysicalClient extends Client
+{
     private String cpf;
     private String rg;
 
-    public ClienteFisico(String cpf, String rg, String nome, Endereco endereco, String telefone, String celular) {
-        super(nome, endereco, telefone, celular);
+    public PhysicalClient(String cpf, 
+    			  String rg, 
+    			  String nome, 
+    			  Address address, 
+    			  String telephone, 
+    			  String cellphone)
+    {
+        super(nome, endereco, telephone, cellphone);
         this.cpf = cpf;
         this.rg = rg;
     }
     
-    public String getCpf() {
+    public String getCpf()
+    {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
+    public void setCpf(String cpf)
+    {
         this.cpf = cpf;
     }
 
-    public String getRg() {
+    public String getRg()
+    {
         return rg;
     }
 
-    public void setRg(String rg) {
+    public void setRg(String rg)
+    {
         this.rg = rg;
-    }
-
-    
-    
+    } 
 }

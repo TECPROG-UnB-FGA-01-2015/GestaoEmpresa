ffa63aab8772e50454d45cf19188c0276c43849c
Matheus S. Pereira <matheussilva.fga@gmail.com>
2015-03-27 19:29:02 -0300

    Writing comments on Fornecedor class

--- src/model/Fornecedor.java
+++ src/model/Fornecedor.java
@@ -10,7 +10,10 @@ public class Fornecedor
 	protected String celular;
 	protected ArrayList<Produto> listaProduto;
 	protected Endereco endereco;
-
+	
+	
+	/* Constructor to receive all the attributes as parameters and initialize the
+	 * object Fornecedor*/
 	public Fornecedor(String nome,
 					  String telefone,
 					  String celular,
@@ -23,52 +26,62 @@ public class Fornecedor
 		this.listaProduto = listaProduto;
 		this.endereco = endereco;
 	}
-
+	
+	// Method to return the content of attribute telefone
 	public String getTelefone()
 	{
 		return telefone;
 	}
-
+	
+	// Method to set a content on variable telefone
 	public void setTelefone(String telefone)
 	{
 		this.telefone = telefone;
 	}
 
+	// Method to return the content of attribute celular
 	public String getCelular()
 	{
 		return celular;
 	}
 
+	// Method to set a content on variable celular
 	public void setCelular(String celular)
 	{
 		this.celular = celular;
 	}
 
+	// Method to return the content of attribute nome
 	public String getNome()
 	{
 		return nome;
 	}
 
+	// Method to set a content on variable nome
 	public void setNome(String nome)
 	{
 		this.nome = nome;
 	}
 
+	// Method to return the content of attribute listaProduto
 	public ArrayList<Produto> getListaProduto()
 	{
 		return listaProduto;
 	}
 
+	// Method to set a content on variable listaProduto
 	public void setListaProduto(ArrayList<Produto> listaProduto)
 	{
 		this.listaProduto = listaProduto;
 	}
 
+	// Method to return the content of attribute endereco
 	public Endereco getEndereco()
 	{
 		return endereco;
 	}
 
+	// Method to set a content on variable endereco
 	public void setEndereco(Endereco endereco)
 	{
 		this.endereco = endereco;

e434f9dced76b9915c8a3d0f54a357627890a823
Matheus S. Pereira <matheussilva.fga@gmail.com>
2015-03-27 19:36:30 -0300

    Writing comments on FornecedorJuridico class

--- src/model/FornecedorJuridico.java
+++ src/model/FornecedorJuridico.java
@@ -8,6 +8,8 @@ public class FornecedorJuridico extends Fornecedor
 	private String razaoSocial;
 	private String cnpj;
 
+	/* Constructor to receive all the attributes as parameters and initialize the
+	 * object FornecedorJuridico*/
 	public FornecedorJuridico(String razaoSocial,
 							  String cnpj,
 							  String nome,
@@ -21,21 +23,25 @@ public class FornecedorJuridico extends Fornecedor
 		this.cnpj = cnpj;
 	}
 
+	// Method to return the content of attribute razaoSocial
 	public String getRazaoSocial()
 	{
 		return razaoSocial;
 	}
 
+	// Method to set a content on variable razaoSocial
 	public void setRazaoSocial(String razaoSocial)
 	{
 		this.razaoSocial = razaoSocial;
 	}
 
+	// Method to return the content of attribute cnpj
 	public String getCnpj()
 	{
 		return cnpj;
 	}
 
+	// Method to set a content on variable cnpj
 	public void setCnpj(String cnpj)
 	{
 		this.cnpj = cnpj;

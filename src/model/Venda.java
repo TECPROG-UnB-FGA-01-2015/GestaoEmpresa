9dbb82d20913df4d9b2a4ff5dc2ad84fe04615a9
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-03-29 12:15:00 -0300

    Adding Comments on each Venda's methods

--- src/model/Venda.java
+++ src/model/Venda.java
@@ -6,6 +6,10 @@ public class Venda extends Transacao
 {
 	private Cliente cliente;
 
+	/* This constructor method is responsible to receive some attributes
+	 * as parameters to initialize the Venda's objects and to call the parent
+	 * constructor (superclass) from Transacao class with its arguments 
+	 */
 	public Venda(Cliente cliente, ArrayList<Produto> listaVenda, double preco,
 			Funcionario funcionario, int dia, int mes, int ano)
 	{
@@ -13,11 +17,13 @@ public class Venda extends Transacao
 		this.cliente = cliente;
 	}
 
+	// This method is responsible to access the cliente's private value
 	public Cliente getCliente()
 	{
 		return cliente;
 	}
 
+	// This method is responsible to modify the cliente's private value
 	public void setCliente(Cliente cliente)
 	{
 		this.cliente = cliente;

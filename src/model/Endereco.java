903212e444405350e8895547b3db8f749c7462a9
Eduardo Moreira <moreira.edu@hotmail.com>
2015-03-30 22:22:29 -0300

    Adding comments to the functions of the class Address

--- src/model/Endereco.java
+++ src/model/Endereco.java
@@ -9,6 +9,7 @@ public class Address
     private String country;
     private String complement;
 
+    // Constructor of the Address' class
     public Address(String street,
                    String number,
                    String city,
@@ -24,61 +25,73 @@ public class Address
         this.complement = complement;
     }
 
+    // Access and returns the property street
     public String getStreet()
     {
         return street;
     }
 
+    // Sets a new value for the street property
     public void setStreet(String street)
     {
         this.street = street;
     }
 
+    // Access and returns the property number
     public String getNumber()
     {
         return number;
     }
 
+    // Sets a new value for the number property
     public void setNumber(String number)
     {
         this.number = number;
     }
 
+    // Access and returns the property city
     public String getCity()
     {
         return city;
     }
 
+    // Sets a new value for the city property
     public void setCity(String city)
     {
         this.city = city;
     }
 
+    // Access and returns the property state
     public String getState()
     {
         return state;
     }
 
+    // Sets a new value for the state property
     public void setState(String state)
     {
         this.state = state;
     }
 
+    // Access and returns the property country
     public String getCountry()
     {
         return country;
     }
 
+    // Sets a new value for the country property
     public void setCountry(String country)
     {
         this.country = country;
     }
 
+    // Access and returns the property complement
     public String getComplement()
     {
         return complement;
     }
 
+    // Sets a new value for the complement property
     public void setComplement(String complement)
     {
         this.complement = complement;

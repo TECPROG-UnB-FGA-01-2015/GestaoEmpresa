da456db4796c32e3c98dac169fa229aee5bfcca1
Eduardo Moreira <moreira.edu@hotmail.com>
2015-03-30 22:22:02 -0300

    Adding comments to the functions of the class PhysicalClient

--- src/model/ClienteFisico.java
+++ src/model/ClienteFisico.java
@@ -5,6 +5,7 @@ public class PhysicalClient extends Client
     private String cpf;
     private String rg;
 
+    // Constructor of the PhysicalClient's class
     public PhysicalClient(String cpf, 
     			  String rg, 
     			  String nome, 
@@ -12,26 +13,30 @@ public class PhysicalClient extends Client
     			  String telephone, 
     			  String cellphone)
     {
-        super(nome, endereco, telephone, cellphone);
+        super(nome, address, telephone, cellphone);
         this.cpf = cpf;
         this.rg = rg;
     }
-    
+
+    // Access and returns the property cpf
     public String getCpf()
     {
         return cpf;
     }
 
+    // Sets a new value for the cpf property
     public void setCpf(String cpf)
     {
         this.cpf = cpf;
     }
 
+    // Access and returns the property rg
     public String getRg()
     {
         return rg;
     }
 
+    // Sets a new value for the rg property
     public void setRg(String rg)
     {
         this.rg = rg;

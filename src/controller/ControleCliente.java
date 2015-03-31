8cbd0ed53ff33d12e09b89ce08b8712c7db6a1d4
Eduardo Moreira <moreira.edu@hotmail.com>
2015-03-30 22:22:48 -0300

    Adding comments to the functions of the class ClientController

--- src/controller/ControleCliente.java
+++ src/controller/ControleCliente.java
@@ -9,58 +9,64 @@ public class ClientController
 {
     ContactView contactView;
     private static ArrayList<Client> clientList;
-    
+
+    // Constructor of the ClientController's class
     public ClientController()
     {
         clientList = new ArrayList<Client>();
     }
 
+    // Access and returns the property clientList
     public static ArrayList<Client> getListaClient()
     {
         return clientList;
     }
 
+    // Sets a new value for the clientList property
     public static void setListaClient(ArrayList<Client> clientList)
     {
         ClientController.clientList = clientList;
     }
     
+    // Adds a client to the clientList
     public void addClient (Client client)
     {
         clientList.add(client);
     }
     
+    // Removes a client of the clientList
     public void removeClient (Client client)
     {
         clientList.remove(client);
     }
     
+    // Search for a client in the clientList with a given name
     public Client searchClient(String name, boolean search)
     {
-        Client return = null;
-        Client exactReturn = null;
+        Client returned = null;
+        Client exactReturned = null;
         int i = 0;
         for(Client client: clientList)
         {
-            if((client.getNome().equalsIgnoreCase(name)))
+            if((client.getName().equalsIgnoreCase(name)))
             {
-                exactReturn = client;
+                exactReturned = client;
                 i = i + 1;
             }
-            else if((client.getNome().toLowerCase().contains(name.toLowerCase())) && search==true)
+            else if((client.getName().toLowerCase().contains(name.toLowerCase())) && search == true)
             {
-                return = client;
+                returned = client;
                 i = i + 1;
             }
         }
         
-        if(exactReturn != null)
+        if(exactReturned != null)
         {
-            return exactReturn;
+            return exactReturned;
         }
-        else if(return != null)
+        else if(returned != null)
         {
-            return return;
+            return returned;
         }
         return null;
     }

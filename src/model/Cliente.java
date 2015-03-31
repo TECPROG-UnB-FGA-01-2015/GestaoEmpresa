d2d782cc07cfa6a63255caeeb2dd83a73ee5b5ff
Eduardo Moreira <moreira.edu@hotmail.com>
2015-03-30 22:21:38 -0300

    Adding comments to the functions of the class Client

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -7,6 +7,7 @@ public class Client
     protected String telephone;
     protected String cellphone;
 
+    // Constructor of the Client's class
     public Client(String name, 
     		  Address address, 
     		  String telephone, 
@@ -18,41 +19,49 @@ public class Client
         this.cellphone = cellphone;
     }
 
-    public String getname()
+    // Access and returns the property name
+    public String getName()
     {
         return name;
     }
 
-    public void setname(String name)
+    // Sets a new value for the name property
+    public void setName(String name)
     {
         this.name = name;
     }
 
+    // Access and returns the property address
     public Address getAddress()
     {
         return address;
     }
 
+    // Sets a new value for the address property
     public void setAddress(Address address)
     {
         this.address = address;
     }
 
+    // Access and returns the property telephone
     public String getTelephone()
     {
         return telephone;
     }
 
+    // Sets a new value for the telephone property
     public void setTelephone(String telephone)
     {
         this.telephone = telephone;
     }
 
+    // Access and returns the property cellphone
     public String getCellphone()
     {
         return cellphone;
     }
 
+    // Sets a new value for the cellphone property
     public void setCellphone(String cellphone)
     {
         this.cellphone = cellphone;

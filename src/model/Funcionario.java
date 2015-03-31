7873678f148750bd8b07d2d59825f17dadff155e
Eduardo Moreira <moreira.edu@hotmail.com>
2015-03-30 22:22:19 -0300

    Adding comments to the functions of the class Employee

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -11,14 +11,15 @@ public class Employee
     private double salary;
     private Address address;
 
+    // Constructor of the Employee's class
     public Employee(String name,
-                       String telephone,
-                       String cellphone,
-                       String cpf,
-                       String rg,
-                       String role,
-                       double salary,
-                       Address address)
+                    String telephone,
+                    String cellphone,
+                    String cpf,
+                    String rg,
+                    String role,
+                    double salary,
+                    Address address)
     {
         this.name = name;
         this.telephone = telephone;
@@ -30,81 +31,97 @@ public class Employee
         this.address = address;
     }
 
+    // Access and returns the property address
     public Address getAddress()
     {
         return address;
     }
 
+    // Sets a new value for the address property
     public void setAddress(Address address)
     {
         this.address = address;
     }
 
+    // Access and returns the property telephone
     public String getTelephone()
     {
         return telephone;
     }
 
+    // Sets a new value for the telephone property
     public void setTelephone(String telephone)
     {
         this.telephone = telephone;
     }
 
+    // Access and returns the property cellphone
     public String getCellphone()
     {
         return cellphone;
     }
 
+    // Sets a new value for the cellphone property
     public void setCellphone(String cellphone)
     {
         this.cellphone = cellphone;
     }
 
+    // Access and returns the property cpf
     public String getCpf()
     {
         return cpf;
     }
 
+    // Sets a new value for the cpf property
     public void setCpf(String cpf)
     {
         this.cpf = cpf;
     }
 
+    // Access and returns the property socialReason
     public String getRg()
     {
         return rg;
     }
 
+    // Sets a new value for the rg property
     public void setRg(String rg)
     {
         this.rg = rg;
     }
-    
+
+    // Access and returns the property name
     public String getName()
     {
         return name;
     }
 
+    // Sets a new value for the name property
     public void setName(String name)
     {
         this.name = name;
     }
 
+    // Access and returns the property role
     public String getRole()
     {
         return role;
     }
 
+    // Sets a new value for the role property
     public void setRole(String role)
     {
         this.role = role;
     }
 
+    // Access and returns the property salary
     public double getSalary()
     {
         return salary;
     }
 
+    // Sets a new value for the salary property
     public void setSalary(double salary)
     {
         this.salary = salary;

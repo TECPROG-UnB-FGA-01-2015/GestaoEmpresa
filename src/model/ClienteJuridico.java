47d93bdc3baa3ee71e4b5a5973bd89851210c994
Eduardo Moreira <moreira.edu@hotmail.com>
2015-03-30 22:21:49 -0300

    Adding comments to the functions of the class JuridicalClient

--- src/model/ClienteJuridico.java
+++ src/model/ClienteJuridico.java
@@ -5,9 +5,10 @@ public class JuridicalClient extends Client
     private String socialReason;
     private String cnpj;
 
+    // Constructor of the JuridicalClient's class
     public JuridicalClient(String socialReason,
                            String cnpj,
-                           String nome,
+                           String name,
                            Address address,
                            String telephone,
                            String cellphone)
@@ -17,21 +18,25 @@ public class JuridicalClient extends Client
         this.cnpj = cnpj;
     }
 
+    // Access and returns the property socialReason
     public String getSocialReason()
     {
         return socialReason;
     }
 
+    // Sets a new value for the socialReason property
     public void setSocialReason(String socialReason)
     {
         this.socialReason = socialReason;
     }
 
+    // Access and returns the property cnpj
     public String getCnpj()
     {
         return cnpj;
     }
 
+    // Sets a new value for the cnpj property
     public void setCnpj(String cnpj)
     {
         this.cnpj = cnpj;

cb3f213b2d2bed9435082095cd5a51e5c6e8879e
Omar Júnior <omarroinuj@gmail.com>
2015-04-02 01:14:45 -0300

    Adding header on Despesa class

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -2,7 +2,6 @@
  * File: Produto.java
  * Purpose: Hold all the attributes and methods of Produto
 **********************************************************/
-		
 package model;
 
 public class Produto

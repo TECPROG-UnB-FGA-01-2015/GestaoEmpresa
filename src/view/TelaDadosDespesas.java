11bb2077b43e8ebb19bfd7c789739f1675a5d196
Omar Júnior <omarroinuj@gmail.com>
2015-04-02 01:23:09 -0300

    Adding header on TelaDadosDespesas class

--- src/view/TelaDadosDespesas.java
+++ src/view/TelaDadosDespesas.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+/**********************************************************
+ * File: TelaDadosDespesas.java
+ * Purpose: Responsible to get the Despesas' informations from user
+**********************************************************/
 package view;
 
 import controller.ControleDespesa;

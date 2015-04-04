7d8546b033a91af6bf9d1d4b74615cb55b61e170
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-04-03 21:27:29 -0300

    Adding Header on Transacao class

--- src/model/Transacao.java
+++ src/model/Transacao.java
@@ -1,9 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+/***********************************************************
+ * File: Transacao.java
+ * Purpose: Responsible to get the Transaction's info 
+ * 			to sell Products
+***********************************************************/
 package model;
 
 import java.util.ArrayList;

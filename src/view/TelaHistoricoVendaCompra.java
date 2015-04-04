a1f639c158854f63627f345cfb83dc49cf7b5290
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-04-03 21:35:34 -0300

    Adding Header on TelaHistoricoVendaCompra class

--- src/view/TelaHistoricoVendaCompra.java
+++ src/view/TelaHistoricoVendaCompra.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+/***********************************************************
+ * File: TelaHistoricoVendaCompra.java
+ * Purpose: Responsible to get the Product's Selling Historic info
+***********************************************************/
 package view;
 
 import java.util.ArrayList;

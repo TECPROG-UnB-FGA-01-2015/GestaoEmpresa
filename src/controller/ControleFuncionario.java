0273c1bafea954a78d940b2d33d32287564a71b2
Eduardo Moreira <moreira.edu@hotmail.com>
2015-04-05 21:52:24 -0300

    Changing functions names to portuguese on ControleFuncionario to fix errors.

--- src/controller/ControleFuncionario.java
+++ src/controller/ControleFuncionario.java
@@ -6,7 +6,6 @@
 package controller;
 
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
 import model.Funcionario;
 import view.TelaContatos;
 
@@ -22,38 +21,38 @@ public class ControleFuncionario
     }
 
     // Access and returns the property employeeList
-    public ArrayList<Funcionario> getEmployeeList() 
+    public ArrayList<Funcionario> getListaFuncionario() 
     {
         return employeeList;
     }
 
     // Sets a new value for the employeeList property
-    public void setEmployeeList(ArrayList<Funcionario> employeeList) 
+    public void setListaFuncionario(ArrayList<Funcionario> employeeList) 
     {
         this.employeeList = employeeList;
     }
     
     // Adds an employee to the employeeList 
-    public void addEmployee(Funcionario funcionario)
+    public void addFuncionario(Funcionario funcionario)
     {
         employeeList.add(funcionario);
     }
     
     // Removes an employee of the employeeList
-    public void removeEmployee(Funcionario funcionario)
+    public void removerFuncionario(Funcionario funcionario)
     {
         employeeList.remove(funcionario);
     }
 
     // Search for an employee in the clientList with a given name
-    public Funcionario searchEmployee(String nome, boolean pesquisa)
+    public Funcionario pesquisarFuncionario(String nome, boolean pesquisa)
     {
     	Funcionario returned = null;
     	Funcionario exactReturned = null;
         int i=0;
         for(Funcionario funcionario: employeeList)
         {
-            if((f.getNome().equalsIgnoreCase(nome)))
+            if((funcionario.getName().equalsIgnoreCase(nome)))
             {
                 exactReturned = funcionario;
                 i = i + 1;

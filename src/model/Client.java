237c658493bf99cee8b13a6059bd57d773709855
Eduardo Moreira <moreira.edu@hotmail.com>
2015-04-12 18:00:04 -0300

    Changing variables names and class name of model Client

---
+++ src/model/Client.java
@@ -0,0 +1,75 @@
+/**********************************************************
+ * File: Client.java
+ * Purpose: Holds all the attributes and methods of the
+ * 		    superclass Client.
+ *********************************************************/		
+
+package model;
+
+public class Client
+{
+    protected String name; 
+    protected Address address;
+    protected String telephone;
+    protected String cellphone;
+
+    // Constructor of the Client's class
+    public Client(String name, 
+    		       Address address, 
+    		       String telephone, 
+    		       String cellphone)
+    {
+        this.name = name;
+        this.address = address;
+        this.telephone = telephone;
+        this.cellphone = cellphone;
+    }
+
+    // Access and returns the property name
+    public String getName()
+    {
+        return name;
+    }
+
+    // Sets a new value for the name property
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    // Access and returns the property address
+    public Address getAddress()
+    {
+        return address;
+    }
+
+    // Sets a new value for the address property
+    public void setAddress(Address address)
+    {
+        this.address = address;
+    }
+
+    // Access and returns the property telephone
+    public String getTelephone()
+    {
+        return telephone;
+    }
+
+    // Sets a new value for the telephone property
+    public void setTelephone(String telephone)
+    {
+        this.telephone = telephone;
+    }
+
+    // Access and returns the property cellphone
+    public String getCellphone()
+    {
+        return cellphone;
+    }
+
+    // Sets a new value for the cellphone property
+    public void setCellphone(String cellphone)
+    {
+        this.cellphone = cellphone;
+    }
+}

e507e85b58408ad5fded65f481a9e2f958e923a3
Eduardo Moreira <moreira.edu@hotmail.com>
2015-04-23 02:06:05 -0300

    Adding else to if on ClientController

--- src/controller/ClientController.java
+++ src/controller/ClientController.java
@@ -58,6 +58,10 @@ public class ClientController
             {
                 returned = client;
             }
+            else
+            {
+            	// Nothing to do
+            }
         }
         
         if(exactReturned != null)

96a849bf1ce58ff36282de306a0b892fa48cb564
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-04-26 23:17:05 -0300

    Making ExpenseController parameters easier to read

--- src/controller/ExpenseController.java
+++ src/controller/ExpenseController.java
@@ -1,7 +1,8 @@
 /**********************************************************
  * File: ExpenseController.java
  * Purpose: Contains the Expense maintenance methods
-**********************************************************/
+ **********************************************************/
+
 package controller;
 
 import java.util.ArrayList;
@@ -9,52 +10,57 @@ import model.Expense;
 
 public class ExpenseController
 {
-    private ArrayList<Expense> expenseList; // Maintains a list of spent of Expense object
+	private ArrayList<Expense> expenseList; // Maintains a list of spent of
+											// Expense object
 
-    // Constructor to instance ControleDespesa with the attribute expenseList 
-    public ExpenseController()
-    {
-        this.expenseList = new ArrayList<Expense>();
-    }
+	// Constructor to instance ControleDespesa with the attribute expenseList
+	public ExpenseController()
+	{
+		this.expenseList = new ArrayList<Expense>();
+	}
 
-    // Method to return the content of attribute expenseList
-    public ArrayList<Expense> getListaGasto()
-    {
-        return expenseList;
-    }
-    
-    // Method to set a content on variable expenseList
-    public void setExpenseList(ArrayList<Expense> expenseList)
-    {
-        this.expenseList = expenseList;
-    }
-    
-    // Method to insert an element which is a Expense type
-    public void addExpense(Expense objectExpense)
-    {
-        expenseList.add(objectExpense);
-    }
-    
-    // Method to remove an element which is a Expense type
-    public void removeExpense(Expense objectExpense)
-    {
-        expenseList.remove(objectExpense);
-    }
-    
-    /* Method to search an element which is a Expense type by passing as a parameter a
-     * name
-     */
-    public Expense searchExpense(String name)
-    {
-    	// Search for the expense
-        for(Expense objectExpense: expenseList)
-        {
-            if(objectExpense.getName().equalsIgnoreCase(name))
-            {
-                return objectExpense;
-            }
-        }
-        return null;
-    }
-   
+	// Method to return the content of attribute expenseList
+	public ArrayList<Expense> getListaGasto()
+	{
+		return expenseList;
+	}
+
+	// Method to set a content on variable expenseList
+	public void setExpenseList(ArrayList<Expense> expenseList)
+	{
+		this.expenseList = expenseList;
+	}
+
+	// Method to insert an element which is a Expense type
+	public void addExpense(Expense objectExpense)
+	{
+		expenseList.add(objectExpense);
+	}
+
+	// Method to remove an element which is a Expense type
+	public void removeExpense(Expense objectExpense)
+	{
+		expenseList.remove(objectExpense);
+	}
+
+	/* Method to search an element which is a Expense type by passing as a
+	 * parameter a name */
+	public Expense searchExpense(String name)
+	{
+		// Search for the expense
+		for (Expense objectExpense : expenseList)
+		{
+
+			String objectExpenseName = objectExpense.getName();
+			boolean objectExpenseNameEquals = objectExpenseName.equalsIgnoreCase(name);
+			String objectExpenseNameToLowerCase = objectExpenseName.toLowerCase();
+
+			if (objectExpenseNameEquals == true)
+			{
+				return objectExpense;
+			}
+		}
+		return null;
+	}
+
 }

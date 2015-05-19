5f94333cea37c63af7883c335203885f0abc80ba
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-05-18 22:47:00 -0300

    Applying logging on StockView

--- src/view/StockView.java
+++ src/view/StockView.java
@@ -9,6 +9,9 @@ package view;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+
+import org.apache.log4j.Logger;
+
 import model.Product;
 import static view.ProductDataView.objectStockController;
 import static view.SalePurchaseView.productMode;
@@ -22,6 +25,8 @@ public class StockView extends javax.swing.JFrame
 	static String codeTable; // Receives an info of the view's table
 	static String tableDescription; // Receives an info of the view's table 
 	static boolean returnProduct = false; // Receives a list of products from the class StockController 
+    static Logger log = Logger.getLogger(StockView.class.getName());
+
 
 	// Constructor to initialize components on StockView
 	public StockView()
@@ -58,6 +63,7 @@ public class StockView extends javax.swing.JFrame
 		ProductDataView.infoCarregar = false;
 		loadList();
 		jTextField_NomeProduto.requestFocus();
+		log.debug("Load StockView");
 	}
 
 	// Method to display a warning message to the user
@@ -69,19 +75,27 @@ public class StockView extends javax.swing.JFrame
 	// Method to load the table with product information
 	public void loadList()
 	{
-		DefaultTableModel model1 = (DefaultTableModel) jTable2.getModel();  
-																			
-		model1.setRowCount(0);
-		jTable2.setModel(model1);
-
-		ArrayList<Product> productList = objectStockController.getProductList();
-		DefaultTableModel model = (DefaultTableModel) jTable2.getModel();
-		model.setRowCount(0);
-		for(Product product : productList)
+		try
 		{
-			model.addRow(new String[] {product.getCode(), product.getDescription(), Double.toString(product.getPurchasePrice()), Double.toString(product.getSellingPrice()), Double.toString(product.getQuantity())});
+			DefaultTableModel model1 = (DefaultTableModel) jTable2.getModel();  
+																				
+			model1.setRowCount(0);
+			jTable2.setModel(model1);
+	
+			ArrayList<Product> productList = objectStockController.getProductList();
+			DefaultTableModel model = (DefaultTableModel) jTable2.getModel();
+			model.setRowCount(0);
+			for(Product product : productList)
+			{
+				model.addRow(new String[] {product.getCode(), product.getDescription(), Double.toString(product.getPurchasePrice()), Double.toString(product.getSellingPrice()), Double.toString(product.getQuantity())});
+			}
+			jTable2.setModel(model);
 		}
-		jTable2.setModel(model);
+		catch(Exception e)
+        {
+        	log.error("Error when loading StockView. Exception: ", e);
+        	throw e;
+        }
 	}
 
 	/**
@@ -116,23 +130,40 @@ public class StockView extends javax.swing.JFrame
 		{
 			public void actionPerformed(java.awt.event.ActionEvent evt)
 			{
-				jButton_PesquisarProdutoActionPerformed(evt);
+				try
+            	{
+					jButton_PesquisarProdutoActionPerformed(evt);
+            	}
+            	catch (Exception e)
+            	{
+            		e.printStackTrace();
+            	}
 			}
 		});
 
 		jTable2.setModel(new javax.swing.table.DefaultTableModel(null, new String[] {"Código", "Descrição", "Preço Compra", "Preço Venda", "Quantidade"})
 		{
+			
 			@Override
 			public boolean isCellEditable(int rowIndex, int mColIndex)
 			{
+				log.info("Load Product's info: code, description, purchase price, selling price and quantity.");
 				return false;
 			}
+			
 		});
 		jTable2.addMouseListener(new java.awt.event.MouseAdapter()
 		{
 			public void mouseClicked(java.awt.event.MouseEvent evt)
 			{
-				jTable2MouseClicked(evt);
+				try
+            	{
+					jTable2MouseClicked(evt);
+            	}
+            	catch (Exception e)
+            	{
+            		e.printStackTrace();
+            	}
 			}
 		});
 		jScrollPane1.setViewportView(jTable2);
@@ -142,7 +173,14 @@ public class StockView extends javax.swing.JFrame
 		{
 			public void actionPerformed(java.awt.event.ActionEvent evt)
 			{
-				jButton_AdicionarProdutoActionPerformed(evt);
+				try
+            	{
+					jButton_AdicionarProdutoActionPerformed(evt);
+            	}
+            	catch (Exception e)
+            	{
+            		e.printStackTrace();
+            	}
 			}
 		});
 
@@ -151,7 +189,14 @@ public class StockView extends javax.swing.JFrame
 		{
 			public void actionPerformed(java.awt.event.ActionEvent evt)
 			{
-				jButton_EditarProdutoActionPerformed(evt);
+				try
+            	{
+					jButton_EditarProdutoActionPerformed(evt);
+            	}
+            	catch (Exception e)
+            	{
+            		e.printStackTrace();
+            	}
 			}
 		});
 
@@ -160,7 +205,14 @@ public class StockView extends javax.swing.JFrame
 		{
 			public void actionPerformed(java.awt.event.ActionEvent evt)
 			{
-				jButton_ExcluirProdutoActionPerformed(evt);
+				try
+            	{
+					jButton_ExcluirProdutoActionPerformed(evt);
+            	}
+            	catch (Exception e)
+            	{
+            		e.printStackTrace();
+            	}
 			}
 		});
 
@@ -169,7 +221,14 @@ public class StockView extends javax.swing.JFrame
 		{
 			public void actionPerformed(java.awt.event.ActionEvent evt)
 			{
-				jButton_CancelarActionPerformed(evt);
+				try
+            	{
+					jButton_CancelarActionPerformed(evt);
+            	}
+            	catch (Exception e)
+            	{
+            		e.printStackTrace();
+            	}
 			}
 		});
 
@@ -178,7 +237,14 @@ public class StockView extends javax.swing.JFrame
 		{
 			public void actionPerformed(java.awt.event.ActionEvent evt)
 			{
-				jButton_ConfirmarProdutoActionPerformed(evt);
+				try
+            	{
+					jButton_ConfirmarProdutoActionPerformed(evt);
+            	}
+            	catch (Exception e)
+            	{
+            		e.printStackTrace();
+            	}
 			}
 		});
 
@@ -238,95 +304,165 @@ public class StockView extends javax.swing.JFrame
 	// Method to search for a product from the name
 	private void jButton_PesquisarProdutoActionPerformed(java.awt.event.ActionEvent evt)
 	{// GEN-FIRST:event_jButton_PesquisarProdutoActionPerformed
-		/// Result of product name and jTextField_NomeProduto text comparison
-		boolean nameProductEquals = "".equals(jTextField_NomeProduto.getText());
-		
-		if(nameProductEquals == true)
+		try
 		{
-			loadList();
-			if(jTable2.getRowCount() == 0)
-			{
-				showMessage("Nenhum produto fora cadastrado até o momento.");
-			}
-		}
-		else
-		{
-			Product otherProduct = objectStockController.searchProduct(jTextField_NomeProduto.getText(), true);
-			DefaultTableModel model1 = (DefaultTableModel) jTable2.getModel();  
+			/// Result of product name and jTextField_NomeProduto text comparison
+			boolean nameProductEquals = "".equals(jTextField_NomeProduto.getText());
 			
-			model1.setRowCount(0);
-			jTable2.setModel(model1);
-
-			ArrayList<Product> testList = objectStockController.getProductList();
-			
-			int sizeOfTestList = testList.size();
-
-			if(sizeOfTestList == 0)
+			if(nameProductEquals == true)
 			{
-				showMessage("Nenhum produto fora cadastrado até o momento.");
-			}
-			else if(otherProduct == null)
-			{
-				showMessage("Produto não encontrado!");
+				loadList();
+				if(jTable2.getRowCount() == 0)
+				{
+					showMessage("Nenhum produto fora cadastrado até o momento.");
+					log.info("No product was added until now.");
+				}
 			}
 			else
 			{
-				model1.addRow(new String[] {otherProduct.getCode(), otherProduct.getDescription(), Double.toString(otherProduct.getPurchasePrice()), Double.toString(otherProduct.getSellingPrice()),
-						Double.toString(otherProduct.getQuantity())});
-				jTextField_NomeProduto.setText(otherProduct.getCode());
+				Product otherProduct = objectStockController.searchProduct(jTextField_NomeProduto.getText(), true);
+				DefaultTableModel model1 = (DefaultTableModel) jTable2.getModel();  
+				
+				model1.setRowCount(0);
+				jTable2.setModel(model1);
+	
+				ArrayList<Product> testList = objectStockController.getProductList();
+				
+				int sizeOfTestList = testList.size();
+	
+				if(sizeOfTestList == 0)
+				{
+					showMessage("Nenhum produto foi cadastrado até o momento.");
+					log.info("No product was registered until now.");
+	
+				}
+				else if(otherProduct == null)
+				{
+					showMessage("Produto não encontrado!");
+					log.info("Product not found!");
+				}
+				else
+				{
+					model1.addRow(new String[] {otherProduct.getCode(), otherProduct.getDescription(), Double.toString(otherProduct.getPurchasePrice()), Double.toString(otherProduct.getSellingPrice()),
+							Double.toString(otherProduct.getQuantity())});
+					jTextField_NomeProduto.setText(otherProduct.getCode());
+				}
 			}
+		}
+		catch(Exception e)
+		{
+			log.error("Error when searching Product. Exception: ", e);
+			throw e; 
 		}
 	}// GEN-LAST:event_jButton_PesquisarProdutoActionPerformed
 
 	// Method to open the view ProductDataView for add a new product
 	private void jButton_AdicionarProdutoActionPerformed(java.awt.event.ActionEvent evt)
 	{// GEN-FIRST:event_jButton_AdicionarProdutoActionPerformed
-		new ProductDataView().setVisible(true);
-		this.setVisible(false);
+		try
+		{
+			new ProductDataView().setVisible(true);
+			this.setVisible(false);
+		}
+		catch(Exception e)
+        {
+        	log.error("Error when adding Product. Exception: ", e);
+        	throw e; 
+        }
 	}// GEN-LAST:event_jButton_AdicionarProdutoActionPerformed
 
 	// Method to cancel the operation and close the screen
 	private void jButton_CancelarActionPerformed(java.awt.event.ActionEvent evt)
 	{// GEN-FIRST:event_jButton_CancelarActionPerformed
-		this.dispose();
-		if(productMode == true)
-			new SalePurchaseView().setVisible(true);
+		try
+		{
+			log.debug("Exit StockView");
+			this.dispose();
+			if(productMode == true)
+			{
+				new SalePurchaseView().setVisible(true);
+			}
+			else
+			{
+				
+			}
+		}
+		catch(Exception e)
+        {
+        	log.error("Error when exiting StockView. Exception: ", e);
+        	throw e; 
+        }
 	}// GEN-LAST:event_jButton_CancelarActionPerformed
 
 	// Method to open the view ProductDataView for edit the product information
 	private void jButton_EditarProdutoActionPerformed(java.awt.event.ActionEvent evt)
 	{// GEN-FIRST:event_jButton_EditarProdutoActionPerformed
-		newProduct = false;
-		new ProductDataView().setVisible(true);
+		try
+		{
+			newProduct = false;
+			new ProductDataView().setVisible(true);
+		}
+		catch(Exception e)
+    	{
+    		log.error("Error when editing Product. Exception: ", e);
+    		throw e;   
+    	}
+		
 	}// GEN-LAST:event_jButton_EditarProdutoActionPerformed
 
 	// Method for Selecting an item in the table and enable the options edit, delete and confirms
 	private void jTable2MouseClicked(java.awt.event.MouseEvent evt)
 	{// GEN-FIRST:event_jTable2MouseClicked
-		DefaultTableModel model = (DefaultTableModel) jTable2.getModel();
-		codeTable = (String) model.getValueAt(jTable2.getSelectedRow(), 0);
-		tableDescription = (String) model.getValueAt(jTable2.getSelectedRow(), 1);
-		jButton_EditarProduto.setEnabled(true);
-		jButton_ExcluirProduto.setEnabled(true);
-		jButton_ConfirmarProduto.setEnabled(true);
+		try
+		{
+			DefaultTableModel model = (DefaultTableModel) jTable2.getModel();
+			codeTable = (String) model.getValueAt(jTable2.getSelectedRow(), 0);
+			tableDescription = (String) model.getValueAt(jTable2.getSelectedRow(), 1);
+			jButton_EditarProduto.setEnabled(true);
+			jButton_ExcluirProduto.setEnabled(true);
+			jButton_ConfirmarProduto.setEnabled(true);
+		}
+		catch(Exception e)
+    	{
+    		log.error("Error when clicking on Product's info. Exception: ", e);
+    		throw e;   
+    	}
 	}// GEN-LAST:event_jTable2MouseClicked
 
 	// Method for removing a product from the table
 	private void jButton_ExcluirProdutoActionPerformed(java.awt.event.ActionEvent evt)
 	{// GEN-FIRST:event_jButton_ExcluirProdutoActionPerformed
-		objectStockController.removeProduct(objectStockController.searchProduct(codeTable, false));
-		loadList();
-		jButton_EditarProduto.setEnabled(false);
-		jButton_ExcluirProduto.setEnabled(false);
+		try
+		{
+			objectStockController.removeProduct(objectStockController.searchProduct(codeTable, false));
+			loadList();
+			log.debug("Product deleted!");
+			jButton_EditarProduto.setEnabled(false);
+			jButton_ExcluirProduto.setEnabled(false);
+		}
+		catch(Exception e)
+    	{
+    		log.error("Error when deleting Product. Exception: ", e);
+    		throw e;   
+    	}
 	}// GEN-LAST:event_jButton_ExcluirProdutoActionPerformed
 
 	// Method to confirm the operation
 	private void jButton_ConfirmarProdutoActionPerformed(java.awt.event.ActionEvent evt)
 	{// GEN-FIRST:event_jButton_ConfirmarProdutoActionPerformed
-		returnProduct = true;
-		productMode = false;
-		this.dispose();
-		new SalePurchaseView().setVisible(true);
+		try
+		{
+			returnProduct = true;
+			productMode = false;
+			log.debug("Product confirmed!");
+			this.dispose();
+			new SalePurchaseView().setVisible(true);
+		}
+		catch(Exception e)
+    	{
+    		log.error("Error when confirming Product. Exception: ", e);
+    		throw e;   
+    	}
 	}// GEN-LAST:event_jButton_ConfirmarProdutoActionPerformed
 
 	/**
@@ -353,23 +489,31 @@ public class StockView extends javax.swing.JFrame
 					javax.swing.UIManager.setLookAndFeel(info.getClassName());
 					break;
 				}
+				else
+				{
+					// Nothing to do
+				}
 			}
 		}
 		catch (ClassNotFoundException ex)
 		{
 			java.util.logging.Logger.getLogger(StockView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			log.error("ClassNotFoundException: ", ex);
 		}
 		catch (InstantiationException ex)
 		{
 			java.util.logging.Logger.getLogger(StockView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			log.error("InstantiationException: ", ex);
 		}
 		catch (IllegalAccessException ex)
 		{
 			java.util.logging.Logger.getLogger(StockView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			log.error("IllegalAccessException: ", ex);
 		}
 		catch (javax.swing.UnsupportedLookAndFeelException ex)
 		{
 			java.util.logging.Logger.getLogger(StockView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			log.error("UnsupportedLookAndFeelException: ", ex);
 		}
 		// </editor-fold>
 
@@ -378,7 +522,15 @@ public class StockView extends javax.swing.JFrame
 		{
 			public void run()
 			{
-				new StockView().setVisible(true);
+				try
+				{
+					new StockView().setVisible(true);
+				}
+				catch(Exception e)
+            	{
+            		log.error("Error when running StockView. Exception: ", e);
+            		throw e;   
+            	}
 			}
 		});
 	}

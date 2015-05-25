671e0e0c226a2298f3eaf5e98b3e030a54deb4bb
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-05-24 22:25:51 -0300

    Adding serialVersionUID to ExpenseDataView

--- src/view/ExpenseDataView.java
+++ src/view/ExpenseDataView.java
@@ -15,6 +15,7 @@ import org.apache.log4j.Logger;
 
 public class ExpenseDataView extends javax.swing.JFrame
 {
+	private static final long serialVersionUID = -3987114343987650026L;
 	// Variables declaration of Buttons, ComboBox, Label and TextFields
     private javax.swing.JButton jButton_Sair; // Button that exits "New Expense" Screen
     private javax.swing.JButton jButton_Salvar; // Button that adds and saves a New Expense on the screen

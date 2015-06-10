213e29918cb58a214e628d27f386722f53e449bc
Omar Junior <omarroinuj@gmail.com>
2015-06-10 09:52:22 -0300

    Standardizing comments on model Sale

--- src/model/Sale.java
+++ src/model/Sale.java
@@ -10,30 +10,28 @@ import java.util.ArrayList;
 
 public class Sale extends Transaction
 {
-	private Client client; // Stores the client object from the Client' class
+	private Client client; // Describes the Client of the sale
 
-	/* This constructor method is responsible to receive some attributes as
-	 * parameters to initialize the Sale's objects and to call the parent
-	 * constructor (superclass) from Transaction class with its arguments */
+	// Constructor to receive all the attributes as parameters and initialize the object Sale
 	public Sale(Client client,
-	            ArrayList<Product> saleList, // Stores the Product's sale list
-	            double price, // Stores the Product's sale price
-	            Employee employee, // Stores the employee object from the Employee' class
-	            int day, // Stores the Product's day sale
-	            int month, // Stores the Product's month sale
-	            int year) // Stores the Product's year sale
+	            ArrayList<Product> saleList, // Describes the sale list of the product
+	            double price, // Describes the price of the sale (value >= 0; example: 74,90)
+	            Employee employee, // Describes the employee of the sale
+	            int day, // Describes the day of the sale (1 <= day <= 31)
+	            int month, // Describes the month of the sale (1 <= day <= 12)
+	            int year) // Describes the year of the sale (year < 0)
 	{
 		super(saleList, price, employee, day, month, year);
 		this.client = client;
 	}
 
-	// This method is responsible to access the Client's private value
+	// Access and returns the Client
 	public Client getClient()
 	{
 		return client;
 	}
 
-	// This method is responsible to modify the Client's private value
+	// Sets a new value for the Client
 	public void setClient(Client client)
 	{
 		this.client = client;

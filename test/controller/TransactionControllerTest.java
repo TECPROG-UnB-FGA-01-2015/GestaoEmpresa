3e9df8b95630fe7cbc22b1a0c2a185106da8291a
Eduardo Moreira <moreira.edu@hotmail.com>
2015-06-11 12:20:03 -0300

    Adding project structure for test files

---
+++ test/controller/TransactionControllerTest.java
@@ -0,0 +1,36 @@
+package controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TransactionControllerTest extends TestCase
+{
+
+	@Before
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	@After
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+	}
+
+	@Test
+	public void testAddSale()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveSale()
+	{
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/model/TransactionTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TransactionTest extends TestCase
+{
+
+	@Before
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	@After
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}

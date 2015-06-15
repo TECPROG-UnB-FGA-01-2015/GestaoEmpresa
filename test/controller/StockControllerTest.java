4f72d42e216fdf89b5b9ec52003a185d5202245d
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-06-15 18:20:48 -0300

    Creating StockControllerTest class

---
+++ test/controller/StockControllerTest.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import model.Product;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StockControllerTest extends TestCase
+{
+	Product product;
+	ArrayList<Product> productList;
+	StockController stockController = new StockController(); 
+
+	@Before
+	public void setUp() throws Exception
+	{
+		product = new Product("1", "Mesas", 900, 5, 1000);
+		productList = new ArrayList<Product>();
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		productList.clear();
+	}
+
+	@Test
+	public void test()
+	{
+		
+	}
+}

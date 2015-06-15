868923cb03dd5e996ee59f2193ae6d87c1a0c501
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-06-15 20:24:26 -0300

    Creating Product Test class on the model test package

---
+++ test/model/ProductTest.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import model.Client;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import junit.framework.TestCase;
+import model.Product;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProductTest extends TestCase
+{
+	Product product;
+	ArrayList<Product> productList;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		product = new Product("1", "Mesas", 900, 5, 1000);
+		productList = new ArrayList<Product>();
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		productList.clear();
+	}
+
+}

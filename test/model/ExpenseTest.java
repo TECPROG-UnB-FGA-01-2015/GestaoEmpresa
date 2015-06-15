3105a7b6cd1ab2ee777d29c6c6353fcd1fc8d9e4
HenriqueAugusto <henriqueaps2003@hotmail.com>
2015-06-15 20:57:11 -0300

    Creating ExpenseTest class on the test package

---
+++ test/model/ExpenseTest.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import model.Expense;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExpenseTest extends TestCase
+{
+	Expense expense;
+	ArrayList<Expense> expenseList;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		expense = new Expense("Foi necessária a compra extra de 50 cadeiras", "Todas as 50 cadeiras são de boa qualidade!", 1000, 15, 6, 2015);
+		expenseList = new ArrayList<Expense>();
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		expenseList.clear();
+	}
+}
